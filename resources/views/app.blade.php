eff809a6cf0508c07a580194ed03d876b1e59f0d
akbrx <strongstroomer@gmail.com>
2025-11-03 21:35:21 +0700

    change icon img

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIMAGANG Diskominfo - Selamat Datang</title>
-    <link rel="icon" type="image/png" href="{{ asset('panel-admin/assets/img/pemkot.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('./pemkot.png') }}">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
